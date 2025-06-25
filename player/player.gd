8ec3b239056f35c951a39b78a05d96e660f93e0e
Violet Budiansky <vivithequeen1@gmail.com>
2025-06-25 14:07:36 -0400

    dashing is better bc it now resets your y velocity and starting to work on dash

--- player/player.gd
+++ player/player.gd
@@ -4,24 +4,29 @@ extends CharacterBody3D
 const SPEED = 12.0
 const MAXSPEED = 8.0;
 const JUMP_VELOCITY = 8
-
 const LOOK_SENSE := 0.0025;
 const dashDistance := 14;
 
-
-var hasJumpedTwice : bool = false
-var dashLocation : Vector3
-var isDashing : bool = false;
-var dashSpeed : float = 1.7;
 @onready var camera : Camera3D = $Camera3D
 
+var hasJumpedTwice : bool = false
+#dash stuff
+var dashLocation : Vector3
+var isDashing : bool = false;
+var dashSpeed : float = 1.54;
 var dashTimer = 0;
+
+#slide
+var slideDirection : Vector3;
+var isSliding : bool = false;
+
+var isMoving : bool = false;
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	$dashLocation.position = Vector3(0*dashDistance, 0, -1*dashDistance)
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
+	if !is_on_floor() && !isDashing:
 		velocity.y += -16 * delta
 
 	# Handle jump.
@@ -39,27 +44,52 @@ func _physics_process(delta):
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	if direction:
+
+
 		$dashLocation.position = Vector3(input_dir.x*dashDistance, 0, input_dir.y*dashDistance)
 	if direction and !isDashing:
+		isMoving = false;
+#		$fovAnimations.play("move_end")
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 #		velocity = clamp(velocity,velocity.length(),Vector3(MAXSPEED,0,MAXSPEED).normalized())
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-	handle_dash(delta);
-	if(Input.is_action_just_pressed("shift")):
-		$fovAnimations.play("dash_start")
-		isDashing = true;
-		dashLocation = $dashLocation.global_position
-
 	
+
+	handle_dash(delta);
+	handle_slide(delta);
 	move_and_slide()
 
 func handle_camera_move(delta : float, input_dir : Vector2):
 	pass
 
+
+
+func handle_slide(delta : float):
+	if(Input.is_action_just_pressed("ctrl")):
+		if(velocity * Vector3(1,0,1)):
+			slideDirection = velocity * Vector3(1,0,1);
+		else:
+			slideDirection = transform.basis*Vector3(0,0,-1) * SPEED
+			
+	if(Input.is_action_pressed("ctrl")):
+		isSliding = true;
+		
+	else:
+		isSliding = false;
+	
+	if(isSliding):
+		velocity.x = slideDirection.x
+		velocity.z = slideDirection.z
+	camera.position.y = 0 if isSliding else 1;
 func handle_dash(delta : float):
+	if(Input.is_action_just_pressed("shift")):
+		velocity.y = 0;
+		$fovAnimations.play("dash_start")
+		isDashing = true;
+		dashLocation = $dashLocation.global_position
 	if(isDashing):
 		dashTimer+=delta*dashSpeed
 		global_position = global_position.lerp(dashLocation,dashTimer)
