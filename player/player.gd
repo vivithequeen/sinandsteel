02ae5b08d67e23a111a6f17c176fdd103aab0cbc
Violet Budiansky <vivithequeen1@gmail.com>
2025-06-25 10:09:43 -0400

    dashing and double jump woring!

--- player/player.gd
+++ player/player.gd
@@ -1,31 +1,45 @@
 extends CharacterBody3D
 
 
-const SPEED = 8.0
+const SPEED = 12.0
 const MAXSPEED = 8.0;
-const JUMP_VELOCITY = 7
+const JUMP_VELOCITY = 12
+
 const LOOK_SENSE := 0.0025;
+const dashDistance := 19;
+
+
+var hasJumpedTwice : bool = false
 var dashLocation : Vector3
 var isDashing : bool = false;
-var dashSpeed : float = 30
+var dashSpeed : float = 10;
 @onready var camera : Camera3D = $Camera3D
 
-
+var dashTimer = 0;
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	$dashLocation.position = Vector3(0*dashDistance, 0, -1*dashDistance)
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += -35 * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or !hasJumpedTwice):
 		velocity.y = JUMP_VELOCITY
+		if(!hasJumpedTwice):
+			hasJumpedTwice = true;
+	if(is_on_floor()):
+		hasJumpedTwice = false;
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "up", "down")
+	
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if direction:
+		$dashLocation.position = Vector3(input_dir.x*dashDistance, 0, input_dir.y*dashDistance)
 	if direction and !isDashing:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -34,11 +48,17 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 	if(isDashing):
+
+		dashTimer+=delta*dashSpeed
 		global_position = lerp(global_position,dashLocation,delta*dashSpeed)
-		if is_equal_approx(global_position.x, dashLocation.x) && is_equal_approx(global_position.z, dashLocation.z):
+		if (dashTimer>=0.99):
+			$fovAnimations.play("dash_end")
+			dashTimer = 0
 			isDashing = false;
 
+
 	if(Input.is_action_just_pressed("shift")):
+		$fovAnimations.play("dash_start")
 		isDashing = true;
 		dashLocation = $dashLocation.global_position
 
