96cbcc216ead201268e8296d4a75895bdb1b87c9
vivithequeen <vivithequeen1@gmail.com>
2025-06-28 10:40:40 -0400

    made improvements to movement

--- player/player.gd
+++ player/player.gd
@@ -6,72 +6,72 @@ const MAXSPEED = 12;
 const JUMP_VELOCITY = 8
 const LOOK_SENSE := 0.0025;
 const dashDistance := 14;
+const GRAVITY := -16.0
 
-@onready var camera : Camera3D = $Camera3D
+@onready var camera: Camera3D = $Camera3D
 
-var hasJumpedTwice : bool = false
+var hasJumpedTwice: bool = false
 #dash stuff
-var dashLocation : Vector3
-var isDashing : bool = false;
-var dashSpeed : float = 1.54;
+var dashLocation: Vector3
+var isDashing: bool = false;
+var dashSpeed: float = 1.54;
 var dashTimer = 0;
 
 
 #slide
-var slideDirection : Vector3;
-var isSliding : bool = false;
+var slideDirection: Vector3;
+var isSliding: bool = false;
 var current_amount_of_dashes = 0
-const AMOUNT_OF_DASHES_MAX : int = 3;
-const SLIDE_MUTI : float = 1.4;
+const AMOUNT_OF_DASHES_MAX: int = 3;
+const SLIDE_MUTI: float = 1.4;
 
 #general movement
-var isMoving : bool = false;
+var isMoving: bool = false;
 
-var hasDashed : bool = false;
-var hasSlided : bool = false;
-var hasWallRun : bool = false;
-var timeOnFloor : float = 0;
-var currentSpeedFov : float = 0;
+var hasDashed: bool = false;
+var hasSlided: bool = false;
+var hasWallRun: bool = false;
+var timeOnFloor: float = 0;
+var currentSpeedFov: float = 0;
 
 #headbob
-var headbobSpeed : float = 6; #headbobs per second
-var headbobTimer : float = 0;
-const headbobLength : float = 0.4;
+var headbobSpeed: float = 6; # headbobs per second
+var headbobTimer: float = 0;
+const headbobLength: float = 0.4;
 
 #wallrun
-var isWallRunning : bool = false
-var wallRunTimer : float = 0
-
+var isWallRunning: bool = false
+var wallRunTimer: float = 0
+var timeWallRunning: float = 0
+const WALLRUN_SPEED_MUTI: float = 1.2
 func _ready():
 	$Camera3D/pixelfilter.size = get_viewport().size
-	print( get_viewport().size)
+	print(get_viewport().size)
 
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	current_amount_of_dashes = AMOUNT_OF_DASHES_MAX
-	$dashLocation.position = Vector3(0*dashDistance, 0, -1*dashDistance)
+	$dashLocation.position = Vector3(0 * dashDistance, 0, -1 * dashDistance)
 func _physics_process(delta):
 
-	if(wallRunTimer>=0):
-		wallRunTimer-=delta
-	var speedMuti : float = 1.0;
-	speedMuti+= 0.2 if hasSlided else 0
-	speedMuti+= 0.2 if hasDashed else 0
-	speedMuti+= 0.2 if hasWallRun else 0
+	var speedMuti: float = 1.0;
+	speedMuti += 0.25 if hasSlided else 0.0
+	speedMuti += 0.25 if hasDashed else 0.0
+	speedMuti += 0.25 if hasWallRun else 0.0
 	# Add the gravity.
 	if !is_on_floor() && !isDashing:
-		velocity.y += -16 * delta
+		velocity.y += GRAVITY * delta
 	
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or !hasJumpedTwice) and !isSliding :
+	if Input.is_action_just_pressed("space") and (is_on_floor() or !hasJumpedTwice) and !isSliding:
 		velocity.y = JUMP_VELOCITY
-		if(isWallRunning):
+		if (isWallRunning):
 			isWallRunning = false;
 			
 			wallRunTimer = 0.3;
-		if(!hasJumpedTwice):
+		if (!hasJumpedTwice):
 			hasJumpedTwice = true;
-	if(is_on_floor()):
-		if(!isSliding):
+	if (is_on_floor()):
+		if (!isSliding):
 			headbob(delta)
 		hasJumpedTwice = false;
 
@@ -82,21 +82,20 @@ func _physics_process(delta):
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	$Camera3D/temp_ui/Label2.text = str(checkIfCanDash())
 	if direction:
-		$dashLocation.position = Vector3(input_dir.x*dashDistance, 0, input_dir.y*dashDistance)
-	if(timeOnFloor >= 0.35 && (is_on_floor()||input_dir.x == 0)):
+		$dashLocation.position = Vector3(input_dir.x * dashDistance, 0, input_dir.y * dashDistance)
+	if (timeOnFloor >= 0.35 && (is_on_floor() || input_dir.x == 0)):
 		speedMuti = 1;
 		hasSlided = false;
 		hasDashed = false;
 		hasWallRun = false;
-	if(is_on_floor()):
-		if(!isSliding and !isDashing):
-			if(timeOnFloor<=0.35):
-				timeOnFloor+=delta
+	if (is_on_floor()):
+		if (!isSliding and !isDashing):
+			if (timeOnFloor <= 0.35):
+				timeOnFloor += delta
 
 	else:
 		timeOnFloor = 0
 	if direction and !isDashing:
-		
 		isMoving = false;
 #		$fovAnimations.play("move_end")
 		velocity.x = direction.x * SPEED * speedMuti
@@ -107,19 +106,19 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 	
-	currentSpeedFov = 90 + (6 if hasSlided else 0) + (6 if hasDashed else 0) +(6 if hasWallRun else 0)+ (15 if input_dir.y !=0 else 0)
-	if(!checkIfCanDash()):
-		if(isDashing):
+	currentSpeedFov = 90 + (8 if hasSlided else 0) + (8 if hasDashed else 0) + (8 if hasWallRun else 0) + (15 if input_dir.y != 0 else 0)
+	if (!checkIfCanDash()):
+		if (isDashing):
 			isDashing = false;
 			dashTimer = 0
 			endDashFov()
-	if(!isDashing):
+	if (!isDashing):
 		runFov();
 		straftCameraTurn(input_dir)
 	handle_dash(delta);
 	handle_slide(delta);
 	handle_wallrun(delta)
-	weaponHold(delta,input_dir)
+	weaponHold(delta, input_dir)
 	print(is_on_floor())
 
 	#velocity.x = clamp(velocity.x,-MAXSPEED * (direction.x if direction.x else 1),MAXSPEED * (direction.x if direction.x else 1))
@@ -127,117 +126,128 @@ func _physics_process(delta):
 	
 	move_and_slide()
 
-func weaponHold(delta,input_dir):
+func weaponHold(delta, input_dir):
 	var tween = get_tree().create_tween()
 	
-	tween.parallel().tween_property($Camera3D/holdingThing,"position:y",sin(headbobTimer)*0.02  + -0.23 + (0 if velocity.y<0 else -0.025) + (0 if velocity.y>0 else 0.025), 0.7)
-	tween.parallel().tween_property($Camera3D/holdingThing,"rotation:z",input_dir.x*-deg_to_rad(9.5) *(-1 if isWallRunning else 1),0.5)
-	tween.parallel().tween_property($Camera3D/holdingThing,"position:z",(0.15 if input_dir.y==1 else 0),0.5)
+	tween.parallel().tween_property($Camera3D/holdingThing, "position:y", sin(headbobTimer) * 0.02 + -0.23 + (0.0 if velocity.y < 0 else -0.025) + (0.0 if velocity.y > 0 else 0.025), 0.7)
+	if(!isSliding):tween.parallel().tween_property($Camera3D/holdingThing, "rotation:z", input_dir.x * -deg_to_rad(9.5) * (-1 if isWallRunning else 1), 0.5)
+	tween.parallel().tween_property($Camera3D/holdingThing, "position:z", (0.15 if input_dir.y == 1 else 0.0), 0.5)
 func headbob(delta):
-	if(velocity.x + velocity.z != 0):
-		headbobTimer += delta * headbobSpeed 
-	if(headbobTimer>PI):
+	if (velocity.x + velocity.z != 0):
+		headbobTimer += delta * headbobSpeed
+	if (headbobTimer > PI):
 		headbobTimer = 0;
 	#var tween = get_tree().create_tween()
 	#tween.tween_property(camera,"position:y",sin(headbobTimer) * headbobLength + 1,0.1)
 
 
-
-func handle_wallrun(delta : float):
+func handle_wallrun(delta: float):
 	$Camera3D/temp_ui/Label.text = str(wallRunTimer)
-	if($raycastleft.is_colliding() && Input.is_action_pressed("left") and wallRunTimer <=0):
-		print("on wall left")
+	if ($raycastleft.is_colliding() && Input.is_action_pressed("left") and !is_on_floor() and wallRunTimer <= 0):
+
 		isWallRunning = true;
-		velocity.y = 0;
-		hasWallRun = true
-	elif($raycastright.is_colliding() && Input.is_action_pressed("right") and wallRunTimer <=0):
-		print("on wall right")
+
+	elif ($raycastright.is_colliding() && Input.is_action_pressed("right") and !is_on_floor() and wallRunTimer <= 0):
+
 		isWallRunning = true;
-		
+
+	else:
+		timeWallRunning = 0;
+		isWallRunning = false;
+	if(isWallRunning):
+		timeWallRunning += delta
 		velocity.y = 0;
 		hasWallRun = true;
-	else:
-		isWallRunning = false;
 
-func handle_slide(delta : float):
-	if(Input.is_action_just_pressed("ctrl") and is_on_floor()):
+		velocity*=WALLRUN_SPEED_MUTI
+
+		velocity.y += GRAVITY / 16.0
+		if(Input.is_action_just_pressed("space")):
+			velocity.y+=JUMP_VELOCITY*1.4
+
+			isWallRunning = false
+			wallRunTimer = 2
+	if(wallRunTimer>=0):
+		wallRunTimer -= delta
+		#velocity.x= (transform.basis * Vector3(1,0,0)).normalized().x * SPEED/16;
+
+func handle_slide(delta: float):
+	if (Input.is_action_just_pressed("ctrl") and is_on_floor()):
 		isSliding = true;
 		hasSlided = true;
-		if(velocity * Vector3(1,0,1)):
-			slideDirection = velocity * Vector3(1,0,1) * SLIDE_MUTI;
+		if (velocity * Vector3(1, 0, 1)):
+			slideDirection = velocity * Vector3(1, 0, 1) * SLIDE_MUTI;
 		else:
-			slideDirection = transform.basis*Vector3(0,0,-1) * SPEED * SLIDE_MUTI
+			slideDirection = transform.basis * Vector3(0, 0, -1) * SPEED * SLIDE_MUTI
 			
-	if(Input.is_action_pressed("ctrl")):
-
+	if (Input.is_action_pressed("ctrl")):
 		pass
 	else:
-		if(!$up_check.is_colliding()):
+		if (!$up_check.is_colliding()):
 			isSliding = false;
 	
-	if(isSliding):
+	if (isSliding):
 		var tween = get_tree().create_tween()
-		tween.tween_property(camera,"rotation:z",(slideDirection.x/abs(slideDirection.x))*-deg_to_rad(7.5) *(-1 if isWallRunning else 1),0.5)
+		tween.tween_property(camera, "rotation:z", (slideDirection.x / abs(slideDirection.x)) * -deg_to_rad(7.5) * (-1 if isWallRunning else 1), 0.5)
 		
 		hasSlided = true;
-		if(Input.is_action_just_pressed("ui_accept") and is_on_floor()):
-			velocity.y +=JUMP_VELOCITY
+		if (Input.is_action_just_pressed("space") and is_on_floor()):
+			velocity.y += JUMP_VELOCITY
 			isSliding = false;
 		velocity.x = slideDirection.x
 		velocity.z = slideDirection.z
 	$CollisionShape3D.shape.height = 1 if isSliding else 2
 	$CollisionShape3D.position.y = -0.5 if isSliding else 0.0
 	
-	if(is_on_floor()):
+	if (is_on_floor()):
 		var tween = get_tree().create_tween()
-		tween.tween_property(camera, "position:y",0 if isSliding else 0.75,0.1)
+		tween.tween_property(camera, "position:y", 0.0 if isSliding else 0.75, 0.1)
 
 
-func handle_dash(delta : float):
-	if(is_on_floor()):
+func handle_dash(delta: float):
+	if (is_on_floor()):
 		current_amount_of_dashes = AMOUNT_OF_DASHES_MAX
-	if(Input.is_action_just_pressed("shift")) and current_amount_of_dashes >0 and checkIfCanDash():
-		current_amount_of_dashes-=1;
+	if (Input.is_action_just_pressed("shift")) and current_amount_of_dashes > 0 and checkIfCanDash() and !isDashing:
+		current_amount_of_dashes -= 1;
 		velocity.y = 0;
-		hasDashed =true
+		hasDashed = true
 		startDashFov()
 		isDashing = true;
 		dashLocation = $dashLocation.global_position
-	if(isDashing):
-		dashTimer+=delta*dashSpeed
-		global_position = global_position.lerp(dashLocation,dashTimer)
-		if (dashTimer>=0.4):
+	if (isDashing):
+		dashTimer += delta * dashSpeed
+		global_position = global_position.lerp(dashLocation, dashTimer)
+		if (dashTimer >= 0.4):
 			endDashFov()
 			dashTimer = 0
 			isDashing = false;
 
-func checkIfCanDash() ->bool:
+func checkIfCanDash() -> bool:
 	for i in $checkcandash.get_children():
-		if(i.is_colliding()):
+		if (i.is_colliding()):
 			return false;
 	return true;
-func straftCameraTurn(input_dir : Vector2):
-	if(!isDashing and !isSliding):
+func straftCameraTurn(input_dir: Vector2):
+	if (!isDashing and !isSliding):
 		var tween = get_tree().create_tween()
-		tween.tween_property(camera,"rotation:z",input_dir.x*-deg_to_rad(7.5) *(-1 if isWallRunning else 1),0.5)
+		tween.tween_property(camera, "rotation:z", input_dir.x * -deg_to_rad(7.5) * (-1 if isWallRunning else 1), 0.5)
 
 func runFov():
 	var tween = get_tree().create_tween()
-	tween.tween_property(camera,"fov",currentSpeedFov,0.5)
+	tween.tween_property(camera, "fov", currentSpeedFov, 0.5)
 
 
 func endDashFov():
 	var tween = get_tree().create_tween()
-	tween.tween_property(camera,"fov",currentSpeedFov,0.1)
+	tween.tween_property(camera, "fov", currentSpeedFov, 0.1)
 
 func startDashFov():
 	var tween = get_tree().create_tween()
-	tween.tween_property(camera,"fov",currentSpeedFov+5,0.1)
+	tween.tween_property(camera, "fov", currentSpeedFov + 5, 0.1)
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		rotation.y+=(-event.relative.x * LOOK_SENSE * 0.5);
-		camera.rotation.x+=(-event.relative.y * LOOK_SENSE)
+		rotation.y += (-event.relative.x * LOOK_SENSE * 0.5);
+		camera.rotation.x += (-event.relative.y * LOOK_SENSE)
 		#camera.rotation.x = clamp(camera.rotation.x, -PI/2 ,PI/2)
-		camera.rotation.x = clamp(camera.rotation.x,-PI/2,PI/2)
-		
+		camera.rotation.x = clamp(camera.rotation.x, -PI / 2, PI / 2)
